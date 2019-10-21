25bb065b47640c5c91d126ef3a3173a4839a9bf9
jprechac <jprechac@gmail.com>
2019-10-21 09:15:44 -0500

    models in upload round 1

--- upload/admin.py
+++ upload/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Song)
+admin.site.register(Artist)
+admin.site.register(Album)
--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+
+    name = models.CharField(max_length=255, blank=False, null=False)
+    image = models.FileField(upload_to='artist_images', blank=True, null=True)
+
+class Album(models.Model):
+
+    name = models.CharField(max_length=255, blank=False, null=False)
+    image = models.FileField(upload_to='album_image', blank=False, null=False)
+    artist = models.ForeignKey(Artist, blank=True, null=True, on_delete=models.SET_NULL)
+
+class Song(models.Model):
+
+    song = models.FileField(upload_to='songs', blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    artist = models.ForeignKey(Artist, blank=False, null=True, on_delete=models.SET_NULL)
+    album = models.ForeignKey(Album, blank=False, null=True, on_delete=models.SET_NULL)
+    duration = models.IntegerField(blank=False, null=False)

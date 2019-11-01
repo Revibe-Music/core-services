c40d6747391b9dcf5f3667f2a5825c7c6d5fa452
Jordan Prechac <jprechac@gmail.com>
2019-11-01 15:49:33 -0500

    trying to get this to work and adding rest auth

--- revibe_api/views.py
+++ revibe_api/views.py
@@ -25,22 +25,22 @@ class ArtistViewSet(viewsets.ModelViewSet):
 class RegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateAccountSerializer
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
+        user = self.get_serializer(data=request.data)
+        user.is_valid(raise_exception=True)
+        user.save()
         return Response({
-            "user": AccountSerializer(user, context=self.get_serializer_context()).data,
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
 
-class LoginAPI(generics.GenericAPIView):
-    #permission_classes = (permissions.AllowAny,)
-    serializer_class = LoginAccountSerializer
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        return Response({
-            "user": AccountSerializer(user, context=self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)[1]
-        })
+# class LoginAPI(generics.GenericAPIView):
+#     #permission_classes = (permissions.AllowAny,)
+#     serializer_class = LoginAccountSerializer
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data
+#         return Response({
+#             "user": AccountSerializer(user, context=self.get_serializer_context()).data,
+#             "token": AuthToken.objects.create(user)[1]
+#         })

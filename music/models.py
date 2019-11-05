d975ccb77b5b994836bd1693e179ce5a12e2920b
Riley Stephens <riley.stephens28@gmail.com>
2019-11-05 14:36:39 -0600

    music model changes and added some serializers

--- music/models.py
+++ music/models.py
@@ -16,43 +16,38 @@ class Album(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, null=False)
     image = models.FileField('Album Image', upload_to='image/albums', null=True)
-    contributors = models.ManyToManyField(Artist, through='AlbumContributors')
+    contributors = models.ManyToManyField(AlbumContributors, through='AlbumContributors')
 
     def __str__(self):
         return "{} ({})".format(self.name, self.id)
 
 class AlbumContributors(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=False)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=False)
     contribution_type = models.CharField(max_length=255, null=True) # limit choices on the application side
 
 class Song(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file = models.FileField('Song', upload_to='audio/songs', null=False)
-    title = models.CharField('Title', max_length=255, null=False)
-    duration = models.DecimalField('Duration', null=True, blank=True, max_digits=6, decimal_places=2) # seconds
     uri = models.CharField('URI', max_length=255, unique=True)
+    file = models.FileField('Song', upload_to='audio/songs', null=False)
+    name = models.CharField('Name', max_length=255, null=False)
     album  = models.ForeignKey(Album, on_delete=models.SET_NULL, null=True, blank=True)
-    # stream = ???
+    duration = models.DecimalField('Duration', null=True, blank=True, max_digits=6, decimal_places=2) # seconds
     platform = models.CharField(max_length=255, null=True)
+    contributors = models.ManyToManyField(SongContributors, through='SongContributors', related_name="song_contributors")
     uploaded_by = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, related_name="song_uploaded_by") # artist or user???
-    uploaded_date = models.DateField(null=True)
-    contributors = models.ManyToManyField(Artist, through='SongContributors', related_name="song_contributors")
-    genre = models.CharField("Genre", max_length=255, null=True, blank=True)
-    beats_per_minute = models.IntegerField("Beats per Minute", null=True, blank=True)
+    uploaded_date = models.DateField(null=True) # need to save the date when object is created
 
     def __str__(self):
         return "{} ({})".format(self.title, self.id)
 
 class SongContributors(models.Model):
-    song = models.ForeignKey(Song, on_delete=models.CASCADE, null=False)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=False)
     contribution_type = models.CharField(max_length=255, null=True) # limit choices on the application side
 
 class Library(models.Model):
-    song = models.ForeignKey(Song, on_delete=models.CASCADE, null=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
     platform = models.CharField(max_length=255, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    songs = models.ManyToManyField(Song)
 
 class Playlist(models.Model):
     name = models.CharField("Name", null=True, blank=False, max_length=255)
--- music/serializers.py
+++ music/serializers.py
@@ -2,12 +2,42 @@ from .models import *
 from rest_framework import serializers
 
 
-class ArtistSerializer(serializers.HyperlinkedModelSerializer):
+class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
-        fields = '__all__'
+        fields = ['id', 'name', 'image', 'platform']
 
-class AlbumSerializer(serializers.HyperlinkedModelSerializer):
+class AlbumContributorSerializer(serializers.ModelSerializer):
+    artist = ArtistSerializer(many=False)
+    class Meta:
+        model = AlbumContributors
+        fields = ['artist', 'contribution_type']
+
+class AlbumSerializer(serializers.ModelSerializer):
+    contributors = AlbumContributorSerializer(many=True)
     class Meta:
         model = Album
+        fields = ['id', 'name', 'image', 'contributors']
+
+class SongContributorSerializer(serializers.ModelSerializer):
+    artist = ArtistSerializer(many=False)
+    class Meta:
+        model = SongContributors
+        fields = ['artist', 'contribution_type']
+
+class SongSerializer(serializers.ModelSerializer):
+    album = AlbumSerializer(many=False)
+    contributors = SongContributorSerializer(many=True)
+    class Meta:
+        model = Song
+        fields = ['id', 'uri', 'name', 'album', 'duration', 'platform', 'contributors']
+
+class LibrarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Library
+        fields = '__all__'
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Playlist
         fields = '__all__'

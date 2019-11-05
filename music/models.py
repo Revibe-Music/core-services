17849e180cbf69adecf1385f89fc2be6ab46739a
jprechac <jprechac@gmail.com>
2019-11-05 15:13:17 -0600

    changes and broken stuff

--- music/models.py
+++ music/models.py
@@ -16,13 +16,14 @@ class Album(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, null=False)
     image = models.FileField('Album Image', upload_to='image/albums', null=True)
-    contributors = models.ManyToManyField(AlbumContributors, through='AlbumContributors')
+    contributors = models.ManyToManyField(Artist, through='AlbumContributors')
 
     def __str__(self):
         return "{} ({})".format(self.name, self.id)
 
 class AlbumContributors(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=False)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=False)
     contribution_type = models.CharField(max_length=255, null=True) # limit choices on the application side
 
 class Song(models.Model):
@@ -33,7 +34,7 @@ class Song(models.Model):
     album  = models.ForeignKey(Album, on_delete=models.SET_NULL, null=True, blank=True)
     duration = models.DecimalField('Duration', null=True, blank=True, max_digits=6, decimal_places=2) # seconds
     platform = models.CharField(max_length=255, null=True)
-    contributors = models.ManyToManyField(SongContributors, through='SongContributors', related_name="song_contributors")
+    contributors = models.ManyToManyField(Artist, through='SongContributors', related_name="song_contributors")
     uploaded_by = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, related_name="song_uploaded_by") # artist or user???
     uploaded_date = models.DateField(null=True) # need to save the date when object is created
 
@@ -42,6 +43,7 @@ class Song(models.Model):
 
 class SongContributors(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=False)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, null=False)
     contribution_type = models.CharField(max_length=255, null=True) # limit choices on the application side
 
 class Library(models.Model):
@@ -53,3 +55,17 @@ class Playlist(models.Model):
     name = models.CharField("Name", null=True, blank=False, max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     songs = models.ManyToManyField(Song)
+
+
+# temp stuff
+class Person(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    members = models.ManyToManyField(Person, through="Membership")
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    reason = models.CharField(max_length=255, null=False)

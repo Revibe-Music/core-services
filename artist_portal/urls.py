0d99e87a4a3dce20a588e36bd1a7e8f9a6f0385c
Riley Stephens <riley.stephens28@gmail.com>
2019-11-15 13:22:52 -0600

    change accounts url to account

--- artist_portal/urls.py
+++ artist_portal/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('accounts/', include('accounts.urls')),
+    path('account/', include('accounts.urls')),
     path('music/', include('music.urls'))
 ]

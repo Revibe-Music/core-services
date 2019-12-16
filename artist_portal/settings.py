410ec9581656d6f8175415233359af196f3b6a83
jprechac <jprechac@gmail.com>
2019-12-15 19:27:59 -0600

    change settings to postgressql

--- artist_portal/settings.py
+++ artist_portal/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'artist_portal.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],

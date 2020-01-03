d397d77aa38c6a551018ddcd53a88bf609187052
jprechac <jprechac@gmail.com>
2020-01-03 14:39:44 -0600

    add production check

--- administration/management/commands/create_schema.py
+++ administration/management/commands/create_schema.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 import MySQLdb
 import os
@@ -8,12 +9,15 @@ import os
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        db = MySQLdb.connect(
-            host=os.environ['RDS_WRITE_HOSTNAME'],
-            user=os.environ['RDS_USERNAME'],
-            password=os.environ['RDS_PASSWORD']
-        )
+        if (not settings.DEBUG) and (settings.USE_S3):
+            db = MySQLdb.connect(
+                host=os.environ['RDS_WRITE_HOSTNAME'],
+                user=os.environ['RDS_USERNAME'],
+                password=os.environ['RDS_PASSWORD']
+            )
 
-        cur = db.cursor()
+            cur = db.cursor()
 
-        cur.execute(f"CREATE SCHEMA {os.environ['RDS_DB_NAME']}")
+            cur.execute(f"CREATE SCHEMA {os.environ['RDS_DB_NAME']}")
+        else:
+            pass

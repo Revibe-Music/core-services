deee168298d898e7f976cf99c63e79a4b60be227
jprechac <jprechac@gmail.com>
2020-01-03 15:30:42 -0600

    put the stuff back

--- revibe/settings.py
+++ revibe/settings.py
@@ -135,7 +135,7 @@ if 'RDS_DB_NAME' in os.environ:
             'default': {},
             'write': {
                 'ENGINE': 'django.db.backends.mysql',
-                'NAME': 'sys',# os.environ['RDS_DB_NAME'],
+                'NAME': os.environ['RDS_DB_NAME'],
                 'USER': os.environ['RDS_USERNAME'],
                 'PASSWORD': os.environ['RDS_PASSWORD'],
                 'HOST': os.environ['RDS_WRITE_HOSTNAME'],
@@ -143,7 +143,7 @@ if 'RDS_DB_NAME' in os.environ:
             },
             'read': {
                 'ENGINE': 'django.db.backends.mysql',
-                'NAME': 'sys', #os.environ['RDS_DB_NAME'],
+                'NAME': os.environ['RDS_DB_NAME'],
                 'USER': os.environ['RDS_USERNAME'],
                 'PASSWORD': os.environ['RDS_PASSWORD'],
                 'HOST': os.environ['RDS_READ_HOSTNAME'],

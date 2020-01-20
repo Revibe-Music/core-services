22dbde42fe8ca7d736477b49a00e26ca302d8b3c
jprechac <jprechac@gmail.com>
2020-01-20 17:06:49 -0600

    add django jet themes

--- revibe/settings.py
+++ revibe/settings.py
@@ -398,8 +398,40 @@ ACCOUNT_EMAIL_REQUIRED = False
 
 # django-jet settings
 JET_DEFAULT_THEME = 'default'
-JET_THEMES = {
-    'theme': 'default',
-    'color': '#8e36a6',
-    'title': 'Revibe Classic'
-}
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'revibe',
+        'color': '#8e36a6', # revoilet
+        'title': 'Revibe Classic'
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]

4b27595a9d49a3659aeef108fe9a7dcbe0d59c19
jprechac <jprechac@gmail.com>
2020-01-27 15:12:08 -0600

    make youtube key model

--- administration/models.py
+++ administration/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 from uuid import uuid1
 
@@ -36,4 +38,34 @@ class Campaign(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     budget = models.IntegerField(null=False, blank=False)
     spent = models.IntegerField(null=True, blank=True)
+
+
+class YouTubeKey(models.Model):
+
+    # id = models.AutoField(primary_key=True)
+    key = models.CharField(
+        help_text=_("API Key"),
+        max_length=255,
+        null=False, blank=False, unique=True
+    )
+
+    last_tested = models.DateTimeField(
+        null=False, blank=True, default=timezone.now
+    )
+    worked_on_last_test = models.BooleanField(
+        help_text=_("The last time this key was tested, it worked."),
+        null=False, blank=True, default=True
+    )
+
+    date_created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        star = "" if self.worked_on_last_test else "*"
+        return f"{star}{self.key}{star}"
+
+    class Meta:
+        verbose_name = 'YouTube Key'
+        verbose_name_plural = 'YouTube Keys'
 

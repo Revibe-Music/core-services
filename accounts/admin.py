443637aee9425ceaf7b6c8de717f38cef824871e
jprechac <jprechac@gmail.com>
2020-01-30 14:44:23 -0600

    more admin changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,26 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    # customize list display
+    list_display = ('__str__', 'date_joined', '_full_name')
+    list_filter = (
+        ('date_joined', admin.DateFieldListFilter),
+        ('is_staff', admin.BooleanFieldListFilter),
+    )
+    # customize actions
+    # actions = [change_password]
+
+    def _full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+
 admin.site.register(Profile)
+
+
 admin.site.register(Social)
+
+
 admin.site.register(ArtistProfile)
--- content/admin.py
+++ content/admin.py
@@ -11,8 +11,12 @@ from content.models import *
 class ArtistAdmin(admin.ModelAdmin):
     # customize list display
     list_display = ('__str__','platform','artist_user')
-    list_filter = ('platform',)
+    list_filter = (
+        'platform',
+        ('date_joined', admin.DateFieldListFilter),
+    )
 
+    # other stuff
     empty_value_display = '-empty-'
 
 
@@ -20,12 +24,13 @@ class ArtistAdmin(admin.ModelAdmin):
 class AlbumAdmin(admin.ModelAdmin):
     # customize list display
     list_display = ('__str__','platform','uploaded_by')
-    list_filter = ('platform','uploaded_by')
+    list_filter = ('platform','uploaded_by',)
     # customize detail display
     # fields = ()
     # customize actions
     actions = [perform_delete, remove_delete]
-    
+
+    # other stuff
     empty_value_display = '-empty-'
 
 
@@ -33,47 +38,73 @@ class AlbumAdmin(admin.ModelAdmin):
 class SongAdmin(admin.ModelAdmin):
     # customize list display
     list_display = ('__str__','platform', 'album','uploaded_by')
-    list_filter = ('platform','album','uploaded_by')
+    list_filter = ('platform','album','uploaded_by',)
     # customize actions
     actions = [perform_delete, remove_delete]
 
+    # other stuff
     empty_value_display = '-empty-'
 
 
 @admin.register(AlbumContributor)
 class AlbumContributorAdmin(admin.ModelAdmin):
+    # customize list display
+    list_display = ('__str__','album', 'artist', 'contribution_type')
+    list_filter = (
+        ('album__uploaded_by', admin.RelatedOnlyFieldListFilter),
+        ('album', admin.RelatedOnlyFieldListFilter),
+        ('artist', admin.RelatedFieldListFilter),
+        'contribution_type',
+        ('primary_artist', admin.BooleanFieldListFilter),
+    )
     # customize actions
     actions = [approve_contribution,]
 
+    # other stuff
     empty_value_display = '-empty-'
 
 
 @admin.register(SongContributor)
 class SongContributorAdmin(admin.ModelAdmin):
+    # customize list display
+    list_display = ('__str__', 'song', 'artist', 'contribution_type')
+    list_filter = (
+        ('song__uploaded_by', admin.RelatedOnlyFieldListFilter),
+        ('song', admin.RelatedOnlyFieldListFilter),
+        ('artist', admin.RelatedFieldListFilter),
+        'contribution_type',
+        ('primary_artist', admin.BooleanFieldListFilter),
+    )
     # customize actions
     actions = [approve_contribution]
 
+    # other stuff
     empty_value_display = '-empty-'
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     # customize list display
-    list_display = ('__str__', 'get_object2')
+    list_display = ('__str__', 'object', 'width', 'height')
+    list_filter = (
+        ('is_original', admin.BooleanFieldListFilter),
+    )
 
+    # other stuff
     empty_value_display = '-empty-'
 
-    def get_object2(self, o):
+    def object(self, o):
         return f"{o.obj.__class__.__name__} - {o.obj}"
 
 
 @admin.register(Track)
 class TrackAdmin(admin.ModelAdmin):
     # customize list display
-    list_display = ('__str__', 'get_object2')
+    list_display = ('__str__', 'song',)
+    list_filter = (
+        ('is_original', admin.BooleanFieldListFilter),
+    )
 
+    # other stuff
     empty_value_display = '-empty-'
-
-    def get_object2(self, o):
-        return f"{o.song.__class__.__name__} - {o.song}"
 

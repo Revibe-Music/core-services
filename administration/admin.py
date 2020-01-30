392fea21b535bdfa6290acb041bfcb7e936c6c14
jprechac <jprechac@gmail.com>
2020-01-30 16:11:09 -0600

    dont remember what I changed

--- administration/admin.py
+++ administration/admin.py
@@ -9,3 +9,12 @@ from administration.models import *
 admin.site.register(ContactForm)
 admin.site.register(Campaign)
 admin.site.register(YouTubeKey)
+
+
+
+# general admin information and changes
+admin.site.empty_value_display = "-empty-"
+admin.site.site_header = "Revibe Administration"
+admin.site.index_title = "Revibe"
+admin.site.site_title = "Revibe Administration"
+admin.site.site_url = "/hc/"

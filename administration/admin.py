aa8a94d6dc61fa095ed3a6bf829ef43c434ca100
Jordan Prechac <jprechac@gmail.com>
2020-02-03 09:22:04 -0600

    change youtube key admin

--- administration/admin.py
+++ administration/admin.py
@@ -7,8 +7,27 @@ from administration.models import *
 # Register your models here.
 
 admin.site.register(ContactForm)
+
+
 admin.site.register(Campaign)
-admin.site.register(YouTubeKey)
+
+
+@admin.register(YouTubeKey)
+class YoutubeKeyAdmin(admin.ModelAdmin):
+    # customize list display
+    list_display = ('sortable_str', 'point_budget',)
+    list_filter = (
+        ('worked_on_last_test', admin.BooleanFieldListFilter),
+    )
+
+    def sortable_str(self, obj):
+        """
+        Provides a field that replicates the classes __str__ method,
+        but also allows for adding a sort field
+        """
+        return obj.__str__()
+    sortable_str.short_description = 'API Key'
+    sortable_str.admin_order_field = 'key'
 
 
 

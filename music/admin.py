fc44f552131cf9d1a2c25c2090781204af058202
Jordan Prechac <jprechac@gmail.com>
2020-02-07 12:28:13 -0600

    edit music admin

--- music/admin.py
+++ music/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
 
+from revibe.admin import html_check_x
+
 from music.models import *
 
-admin.site.register(Library)
-admin.site.register(Playlist)
-admin.site.register(LibrarySong)
-admin.site.register(PlaylistSong)
+# -----------------------------------------------------------------------------
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    # customize list display
+    list_display = ('__str__', 'user')
+    list_filter = ('platform',)
+
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    # customize list display
+    list_display = ('__str__', 'user', 'html_public')
+    list_filter = (
+        ('is_public', admin.BooleanFieldListFilter),
+    )
+
+    # customize search
+    search_fields = ['user__username', 'name']
+
+    def html_public(self, obj):
+        return html_check_x(obj, 'is_public')
+    html_public.short_description = 'public status'
+    html_public.admin_order_field = 'is_public'
+
+
+@admin.register(LibrarySong)
+class LibrarySongAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PlaylistSong)
+class PlaylistSongAdmin(admin.ModelAdmin):
+    pass
+

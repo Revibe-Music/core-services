c962e9a946f61455b9ae17b302e29d02ec293ef7
Jordan Prechac <jprechac@gmail.com>
2020-02-21 14:47:10 -0600

    add try except to deleting objects from s3

--- revibe/utils/aws/s3.py
+++ revibe/utils/aws/s3.py
@@ -6,8 +6,10 @@ Functions that make it easy to operate with objects in S3
 """
 
 from django.conf import settings
+from rest_framework.exceptions import APIException
 
 import boto3
+from boto3.s3.con
 
 # -----------------------------------------------------------------------------
 
@@ -21,6 +23,9 @@ def delete_s3_object(file, *args, **kwargs):
     s3_string = f"media/{file.name}"
 
     # get s3
-    s3 = boto3.resource('s3')
-    s3.Object(settings.AWS_STORAGE_BUCKET_NAME, s3_string).delete()
+    try:
+        s3 = boto3.resource('s3')
+        s3.Object(settings.AWS_STORAGE_BUCKET_NAME, s3_string).delete()
+    except Exception as e:
+        raise APIException(str(e))
 

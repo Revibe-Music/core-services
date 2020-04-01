c22daad809982cf252986b27968165e2aa65bddd
Jordan Prechac <jprechac@gmail.com>
2020-04-01 14:06:27 -0500

    extend stream and search admin pages

--- metrics/admin.py
+++ metrics/admin.py
@@ -6,12 +6,42 @@ from metrics.models import *
 
 @admin.register(Stream)
 class StreamAdmin(admin.ModelAdmin):
-    pass
+    # customize list display
+    list_display = (
+        '__str__',
+        'song',
+        'user',
+        'source',
+    )
+    list_filter = (
+        'source',
+        ('song', admin.RelatedOnlyFieldListFilter),
+        ('user', admin.RelatedOnlyFieldListFilter),
+        'alternate_platform',
+        ('timestamp', admin.DateFieldListFilter),
+        ('is_downloaded', admin.BooleanFieldListFilter),
+        ('is_saved', admin.BooleanFieldListFilter),
+    )
+
+    # customize search
+    search_fields = [
+        'song__title',
+        'user__username'
+    ]
 
 
 @admin.register(Search)
 class SearchAdmin(admin.ModelAdmin):
-    pass
+    # customize list display
+    list_display = (
+        '__str__',
+        'user',
+    )
+    list_filter = (
+        'search_text',
+        ('user', admin.RelatedOnlyFieldListFilter),
+        ('timestamp', admin.DateFieldListFilter)
+    )
 
 
 @admin.register(AppSession)

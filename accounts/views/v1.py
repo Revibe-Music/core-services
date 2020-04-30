706fafbe8778816e3d18cfbb2dd76bcf3f4512c3
Jordan Prechac <jprechac@gmail.com>
2020-04-30 12:48:38 -0500

    configure callback url for google for all envs

--- accounts/views/v1.py
+++ accounts/views/v1.py
@@ -413,8 +413,25 @@ class GoogleLogin(SocialLoginView):
     Creates user from Google profile or logs user in from Google profile
     """
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://127.0.0.1:8000/v1/account/google-authentication/callback/"
     client_class = OAuth2Client
+
+    @property
+    def callback_url(self):
+        method = "http://"
+        root = ""
+        if settings.DEBUG == False:
+            # production
+            method = "https://"
+            root = "api.revibe.tech"
+        elif settings.USE_S3 == True:
+            # test
+            root = "test-env.myrpupud2p.us-east-2.elasticbeanstalk.com"
+        else:
+            # local
+            root = "127.0.0.1:8000"
+        
+        return f"{method}{root}/v1/account/google-authentication/callback/"
+
 
 
 # Linked Account Views

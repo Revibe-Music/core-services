90ce4475d1e485eb6aa44822487df96d1d382a95
Jordan Prechac <jprechac@gmail.com>
2020-05-19 10:54:56 -0500

    add test notification thingy

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -3,7 +3,10 @@ Created: 08 May 2020
 Author: Jordan Prechac
 """
 
+from django.core.mail import send_mail
 from celery import shared_task
+
+import datetime
 
 from accounts.models import CustomUser
 from .core import Notifier
@@ -19,4 +22,23 @@ def send_notification(user_id, trigger, *args, **kwargs):
 
     return result
 
+@shared_task
+def test_send_notification():
+    """
+    Sends an email to 
+    """
+    to_address = "dev@revibe.tech"
+    subject = "Celery Beat Confirmation"
+    message = "If you are reading this email then Celery Beat is working in Production.\n\n" + \
+        f"Execution time: {datetime.datetime.now()}"
+    from_email = "noreply@revibe.tech"
+
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=from_email,
+        recipient_list=[to_address,]
+    )
+
+    return
 

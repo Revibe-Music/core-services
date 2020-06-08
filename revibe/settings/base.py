b65150fe841cde97ab3da72990bebe6a81834432
Jordan Prechac <jprechac@gmail.com>
2020-06-08 15:35:24 -0500

    update version

--- revibe/settings/base.py
+++ revibe/settings/base.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'kqjvskii-n3exogny_&n7$lm)1w#_^16$a$zhdc7r*&#6(g=sf'
-VERSION = '1.1.4'
+VERSION = '1.1.5'
 
 INSTALLED_APPS = [
     # cors stuff

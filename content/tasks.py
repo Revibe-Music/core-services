afc2e142d4f0e383e7bc57548958a9b8f08374bf
Jordan Prechac <jprechac@gmail.com>
2020-06-18 15:28:45 -0500

    pulling at straws here

--- content/tasks.py
+++ content/tasks.py
@@ -3,7 +3,7 @@
 
 from celery import shared_task
 
-from revibe.utils.mail import error_email
+# from revibe.utils.mail import error_email
 
 from administration.models import ContactForm
 
@@ -18,12 +18,12 @@ def convert_track_task(song_id, *args, **kwargs):
         if not bool(worked):
             raise ValueError("Bad stuff happened in there...")
     except Exception as e:
-        error_email(
-            "jordanprechac@revibe.tech",
-            "celery.task.convert_track_task",
-            e,
-            False
-        )
+        # error_email(
+        #     "jordanprechac@revibe.tech",
+        #     "celery.task.convert_track_task",
+        #     e,
+        #     False
+        # )
 
         ContactForm.objects.create(subject="Failed Celery Song Processing", message=f"Song id: {song_id}")
 
--- revibe/celery.py
+++ revibe/celery.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
-from django.conf import settings
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.

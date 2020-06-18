d98863e3f24383332156e2eccaf336494f094ba8
Jordan Prechac <jprechac@gmail.com>
2020-06-18 14:10:31 -0500

    auto discover tasks from installed apps

--- revibe/celery.py
+++ revibe/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
+from django.conf import settings
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
@@ -17,7 +18,7 @@ app.config_from_object('django.conf:settings', namespace="CELERY") # only for ce
 # app.config_from_object('django.conf:settings') # for celery < 4.0
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
